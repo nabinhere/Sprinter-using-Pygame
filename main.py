2e1deae58e9d0ec3a4386dc477137e37e7e75898
Nabin <shnabin2056@gmail.com>
2023-06-11 18:09:35 +0545

    added sky, ground and text surfaces

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import pygame
 
 def main():
+
+    # initialize pygame
     pygame.init()
 
     # logo = pygame.image.load("image.extension")
     # pygame.display.set_icon(logo)
+    # set caption
     pygame.display.set_caption("Runner")
-    screen = pygame.display.set_mode((560, 400))
+
+    # create an empty window of size 800X400
+    screen = pygame.display.set_mode((800, 400))
+
+    # setup font
+    test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
+
     clock = pygame.time.Clock()
+
+    sky_surface = pygame.image.load("graphics/sky.png")
+    ground_surface = pygame.image.load("graphics/ground.png")
+    text_surface = test_font.render("My game", False, "black")
 
     running = True
     while running:
@@ -15,7 +28,11 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-    clock.tick(60)
+        screen.blit(sky_surface, (0,0))
+        screen.blit(ground_surface, (0, 300))
+        screen.blit(text_surface, (300, 50))
+        pygame.display.update()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
