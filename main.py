b3b0c07ce3c80eb462ceff0679aa719e4c6ac20d
Nabin <shnabin2056@gmail.com>
2023-06-25 08:50:57 +0545

    added gravity to the player

--- main.py
+++ main.py
@@ -12,14 +12,15 @@ def main():
     screen = pygame.display.set_mode((800, 400))
 
     # setup font
-    test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
+    font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
     # set up clock to use for configuring the frame rate
     clock = pygame.time.Clock()
 
     sky_surface = pygame.image.load("graphics/sky.png").convert()
     ground_surface = pygame.image.load("graphics/ground.png").convert()
-    text_surface = test_font.render("My game", False, "black")
+    score_surf = font.render("Snail Sprint", False, (64,64,64))
+    score_rect = score_surf.get_rect(centerx = 400, centery = 50)
 
     # Creating a Snail rectangle
     snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
@@ -28,24 +29,36 @@ def main():
     # Creating a player rectangle
     player_surf = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
     player_rect = player_surf.get_rect(midbottom = (80, 300))
+    player_gravity = 0
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player_gravity = -20
+                
 
         # static part/images on the screen
         screen.blit(sky_surface, (0,0))
         screen.blit(ground_surface, (0, 300))
-        screen.blit(text_surface, (300, 50))
+        pygame.draw.rect(screen, "#c0e8ec", score_rect)
+        pygame.draw.rect(screen, "#c0e8ec", score_rect, 6)
+        screen.blit(score_surf, score_rect)
 
         # dynamic part of the screen
         snail_rect.x -= 4
         if snail_rect.right <=0: snail_rect.left = 800 
         screen.blit(snail_surf, snail_rect)
+
+        player_gravity +=1
+        player_rect.bottom += player_gravity
         screen.blit(player_surf, player_rect)
 
+                                                                           
         pygame.display.update()
         clock.tick(60)  #sets frame rate to 60, i.e, changes the images/surfaces 60 times per second
 
